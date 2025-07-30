3af618d8256f3816367f9b7ead32b3f04dd7149e
rghod <raghadsaad.gha@gmail.com>
2025-07-31 01:32:30 +0300

    Add frontend views

---
+++ waznTests/waznTests.swift
@@ -0,0 +1,17 @@
+//
+//  waznTests.swift
+//  waznTests
+//
+//  Created by Raghad  on 01/02/1447 AH.
+//
+
+import Testing
+@testable import wazn
+
+struct waznTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
